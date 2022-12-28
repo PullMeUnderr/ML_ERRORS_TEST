79f72b6e57252fc3b5adcd1a64681d6bb5bd2ce8
vlad1 <vlad109108@gmil.com>
2022-12-28 17:25:15 +0300

    test

--- main.py
+++ main.py
@@ -2,25 +2,24 @@ import openpyxl
 
 error_list_xlsx = openpyxl.open("Spisok_oshibok_ML_dlya_programmy_Python.xlsx", read_only=True)
 
-sheet = error_list_xlsx.active
-error_list = sheet
-error_text = ''
+error_list = error_list_xlsx.active
+while True:
+    print("Введите номер вашей ошибки:")
+    number = int(input())
 
 
-print("Введите номер вашей ошибки:")
-number = int(input())
+    def error_search(error_number):
+        flag = False
+        for i in range(1, error_list.max_row + 1):
+            error_line = error_list[i][0].value
+            if str(error_number) in error_line[:6] and str(error_number) == error_line[:6]:
+                error_text = error_list[i][0].value
+                flag = True
+                return f"Ваша ошибка:{error_text[6:].title()}"
 
+        if flag == False:
+            return "Такой ошибки не существует!"
 
-def error_search(error_number):
-    flag = False
-    for i in range(1, error_list.max_row + 1):
-        error_line = error_list[i][0].value
-        if str(error_number) in error_list[i][0].value and str(error_number) == error_line[:6]:
-            error_text = error_list[i][0].value
-            flag = True
-            return f"Ваша ошибка:{error_text[6:].title()}"
-    if flag == False:
-        return "Такой ошибки не существует!"
-
-
-print(error_search(number))
+    print()
+    print(error_search(number))
+    print()
