f9d2ac523ddef2f79d1cee212c8cd0f8e26ec56c
PullMeUnderr <vlad109108@gmail.com>
2022-12-31 22:00:13 +0300

    Добавлена проверка на подлинность числа ошибки

--- main.py
+++ main.py
@@ -5,17 +5,19 @@ error_list_xlsx = openpyxl.open("Spisok_oshibok_ML_dlya_programmy_Python.xlsx", 
 error_list = error_list_xlsx.active
 while True:
 
-    number = int(input("Введите номер вашей ошибки: "))
+    number = input("Введите номер вашей ошибки: ")
 
 
     def error_search(error_number):
         flag = False
         for i in range(1, error_list.max_row + 1):
             error_line = error_list[i][0].value
-            if str(error_number) in error_line[:6] and str(error_number) == error_line[:6]:
-                error_text = error_list[i][0].value
-                flag = True
-                return f"Ваша ошибка:{error_text[6:].title()}"
+            if str(error_number).isdigit() == False:
+                return "Это не номер ошибки"
+            if str(error_number) in error_line[:6] and str(error_number) == error_line[:6] and str(error_number).isdigit() == True:
+                    error_text = error_list[i][0].value
+                    flag = True
+                    return f"Ваша ошибка:{error_text[6:].title()}"
 
         if flag == False:
             return "Такой ошибки не существует!"
